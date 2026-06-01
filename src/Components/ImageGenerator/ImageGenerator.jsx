a70d0ffa6c99dac19060ff33c9917388f5ee0271
Manish Darshan D <manishdarshan2013@gmail.com>
2026-06-01 14:56:02 +0530

    Initial commit

---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+import React, { useRef, useState } from 'react'
+import './ImageGenerator.css'
+import default_image from '../Assets/default_image.svg'
+
+const ImageGenerator = () => {
+
+  const [image_url, setImage_url] = useState("/");
+  let inputRef = useRef(null);
+
+  const imageGenerator = async() => {
+    if (inputRef.current.value==="") {
+      return 0;
+    }
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers:{
+        "Content-Type": "application/json",
+        Authorization:
+        process.env.REACT_APP_OPENAI_API_KEY,
+        "User-Agent":"Chrome",
+        },
+        body:JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n:1,
+          size:"512*512",
+        }), 
+      }
+    );
+
+    let data = await response.json();
+    console.log(data);
+  }
+
+  return (
+    <div className='ai-image-generator'>
+     <div className="header">Ai image <span>generator</span></div>
+     <div className="image-loading">
+      <div className="image"><img src={image_url==="/"?default_image:image_url} alt="" /></div>
+      </div>
+      <div className="search-box">
+        <input type="text" ref={inputRef} className="search-input" placeholder="Describe What You Want To See" />
+        <div className="generate-btn" onClick={()=>{imageGenerator()}}>Generate</div>
+      </div>
+    </div>
+  )
+}
+
+export default ImageGenerator
